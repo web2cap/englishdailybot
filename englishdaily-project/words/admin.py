de41c9c0d029475db68642c2334f026a1ddc6b7d
Pavel <web2cap@gmail.com>
2023-01-18 20:20:32 +0100

    Word, List, Subscription models and admin

---
+++ englishdaily-project/words/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Word, WordList, WordListSubscriplion
+
+admin.site.register(Word)
+admin.site.register(WordList)
+admin.site.register(WordListSubscriplion)
---
+++ englishdaily-project/words/models.py
@@ -0,0 +1,157 @@
+from django.db import models
+
+from users.models import User
+
+
+class WordList(models.Model):
+
+    ACCESS_CHOICES = ((False, "Private"), (True, "Public"))
+
+    name = models.CharField(
+        "Word List name",
+        max_length=256,
+        null=False,
+        blank=False,
+    )
+
+    name_ru = models.CharField(
+        "Russian Word List name",
+        max_length=256,
+        null=True,
+        blank=True,
+        default=None,
+    )
+
+    description = models.TextField(
+        "Word List description", blank=True, null=True, default=None
+    )
+
+    author = models.ForeignKey(
+        User,
+        verbose_name="Word List author",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="word_lists",
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Adding date and time"
+    )
+
+    access = models.BooleanField(
+        "Public access",
+        choices=ACCESS_CHOICES,
+        default=True,
+        blank=False,
+        null=False,
+    )
+
+    native = models.BooleanField(
+        "Native community word list",
+        blank=False,
+        null=False,
+        default=False,
+    )
+
+    # TODO: on save parse
+    parse = models.TextField(
+        "Add many words.",
+        blank=True,
+        null=True,
+        default=None,
+        help_text="For a quick addition, add one word per line in English.",
+    )
+
+    class Meta:
+        verbose_name = "Words list"
+        verbose_name_plural = "Words lists"
+        ordering = ["-native", "author", "name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "author"], name="unique_name_by_author"
+            )
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Word(models.Model):
+
+    en = models.CharField(
+        "English word",
+        max_length=128,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    ru = models.CharField(
+        "Russian translation",
+        max_length=128,
+        null=False,
+        blank=False,
+    )
+
+    list = models.ManyToManyField(
+        WordList, verbose_name="Words list", related_name="words"
+    )
+
+    example = models.TextField(
+        "Example of usage", blank=True, null=True, default=None
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Adding date and time"
+    )
+
+    class Meta:
+        verbose_name = "Word"
+        verbose_name_plural = "Words"
+        ordering = ["en"]
+
+    def __str__(self):
+        return self.en
+
+
+class WordListSubscriplion(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="word_list_subscriplion",
+        verbose_name="Follower",
+        null=False,
+        blank=False,
+    )
+
+    list = models.ForeignKey(
+        WordList,
+        on_delete=models.CASCADE,
+        related_name="word_list_subscriplion",
+        verbose_name="Word list",
+        null=False,
+        blank=False,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Subscriplion date and time"
+    )
+
+    rate = models.PositiveIntegerField(
+        "Daily rate", null=True, blank=False, default=None
+    )
+
+    class Meta:
+        verbose_name = "Word-list subscriplion"
+        verbose_name_plural = "Word-list subscriplions"
+        ordering = ["user", "-created"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "list"], name="unique_subscriplion"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} by {self.list}"
