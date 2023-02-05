6849cd35b205dcbce99d7cc22c2f74956b50f8e7
Pavel <web2cap@gmail.com>
2023-02-05 14:10:39 +0100

    refactor app and model collection

--- englishdaily-project/englishdaily/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path(f"bot/", include("bot.urls")),
-]
--- englishdaily-project/words/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WordsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "words"
